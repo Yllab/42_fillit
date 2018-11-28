e8d20ec7c8e2c6fa48fb1f8cf0f67c37c00aee04
hbally <hbally@student.42.fr>
2018-11-28 18:33:53 +0100

    add protections in main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/24 17:56:51 by hbally            #+#    #+#             */
-/*   Updated: 2018/11/28 17:35:49 by hbally           ###   ########.fr       */
+/*   Updated: 2018/11/28 18:32:09 by hbally           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,17 @@ int				main(int argc, char **argv)
 	int			ret;
 	t_short 	*tetro;
 
-	fd = open(argv[1], O_RDONLY);
-	tetro = get_input(fd);
-	if (!tetro)
-		print_error();
-	if (move_tetros_pilot(tetro) == -1)
-		print_error();
+	if (!(fd = open(argv[1], O_RDONLY) == -1))
+	{
+		tetro = get_input(fd);
+		if (tetro)
+		{
+			if (move_tetros_pilot(tetro) == -1)
+				print_error();
+			ft_memdel(&tetro);
+			return (0);
+		}
+	}
+	print_error();
 	return (0);
 }
